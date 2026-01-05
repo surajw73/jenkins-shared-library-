62fd81fe0fbcc149d981ca9c3582c74274742203
surajw73 <144908668+surajw73@users.noreply.github.com>
2026-01-05 15:45:11 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker buildex build --platform linux/amd64 -t "${DockerHubUser}"/"${ProjectName}":"${ImageTag}" --load  . "
+  sh "docker buildex build -t "${DockerHubUser}"/"${ProjectName}":"${ImageTag}" . "
 }

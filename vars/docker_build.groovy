091ab2aa272946ce5c3f8a73512ea16a803289e2
surajw73 <144908668+surajw73@users.noreply.github.com>
2026-01-05 16:54:26 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,8 +1,9 @@
 def call(Map config = [:]) {
     def imageName = config.imageName ?: error('enter image name')
     def imageTag = config.imageTag ?: 'latest'
+    def  dockerHubUser = config.dockerHubUser ?: error('enter docker hub user namne' )
     def dockerfile = config.dockerfile ?: 'Dockerfile'
     def context = config.context ?: '.'
-    def  dockerHubUser = config.dockerHubUser ?: error('enter docker hub user namne' )
+   
   sh "docker build tag "${dockerHubUser}"/"${imageName}":"${imageTag}" -f "${dockerfile}" "${context}" "
 }

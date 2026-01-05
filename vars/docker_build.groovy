4f5e68cb836dc22893cbfe5f96df9ef394b10b03
surajw73 <144908668+surajw73@users.noreply.github.com>
2026-01-05 16:08:34 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,9 @@
+def call(Map config = [:]) {
+    def imageName = config.imageName ?: 'ProjectName'
+    def imageTag = config.imageTag ?: 'latest'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def context = config.context ?: '.'
+
 def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build tag "${DockerHubUser}"/"${ProjectName}":"${ImageTag}" . "
+  sh "docker build tag "${DockerHubUser}"/"${ProjectName}":"${ImageTag}" -f "${dockerfile}" "${context}" "
 }
